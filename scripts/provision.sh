31799843fd5f6fce764656f41cd1bd428bd2accf
Stenio Ferreira <stenio123@gmail.com>
2018-03-10 09:03:23 -0600

    Revert "Merge pull request #3 from stenio123/custom-config"
    This reverts commit b6e620c6ff0ca55e898a1c9af962e216f15d838f, reversing
    changes made to aa23fd765e244fb1dbf3a13b6c8a2cd67d108f1a.
    
    Fixing wrong merge

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -9,8 +9,13 @@ function provision() {
   for f in $(ls "$1"/*.json); do
     p="$1/${f%.json}"
     echo "Provisioning $p"
-    # echo runnin curl --location --fail --header "X-Vault-Token: ${VAULT_TOKEN}" --data @"${f}" "${VAULT_ADDR}/v1/${p}"
-    curl --silent --location --fail --header "X-Vault-Token: ${VAULT_TOKEN}" --data @"${f}" "${VAULT_ADDR}/v1/${p}"
+    curl \
+      --silent \
+      --location \
+      --fail \
+      --header "X-Vault-Token: ${VAULT_TOKEN}" \
+      --data @"${f}" \
+      "${VAULT_ADDR}/v1/${p}"
   done
   popd > /dev/null
   set -e
@@ -19,15 +24,11 @@ function provision() {
 echo "Verifying Vault is unsealed"
 vault status > /dev/null
 
-pushd ../data >/dev/null
+pushd data >/dev/null
 provision sys/auth
 provision sys/mounts
 provision sys/policy
 provision postgresql/config
 provision postgresql/roles
 provision auth/userpass/users
-provision secret/app1/dev
-provision secret/app1/prod
-provision secret/app2/dev
-provision secret/app2/prod
 popd > /dev/null
